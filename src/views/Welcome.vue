51b56b4bb44c1babb69ffc0fd29fbdf4f2e4901e
nomikhokher <nomankhokher786@gmail.com>
2022-07-20 19:51:23 +0000

    --some -tweaks

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="grid h-screen place-items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-        <p class="text-4xl underline decoration-wavy decoration-2 underline-offset-8 whitespace-normal text-white">Hi, This will be the start of something great!</p>
+        <div class="text-white">
+            <h1 class="text-7xl text-center">{{'<Application />'}}</h1>
+            <p class="text-4xl underline decoration-wavy decoration-2 underline-offset-8 whitespace-normal">This will be the start of something great!</p>
+        </div>
     </div>
 </template>
