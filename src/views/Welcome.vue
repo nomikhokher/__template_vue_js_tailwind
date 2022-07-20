3be59eb97336e1b8dee69c28b5f75bec45c14bbc
nomikhokher <nomankhokher786@gmail.com>
2022-07-20 19:46:54 +0000

    --set-up views/display

---
+++ src/views/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="grid h-screen place-items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <p class="text-4xl underline decoration-wavy decoration-2 underline-offset-8 whitespace-normal text-white">Hi, This will be the start of something great!</p>
+    </div>
+</template>
