8dacc0df906d6abf752afb40ed46e02487cde30c
nomikhokher <nomankhokher786@gmail.com>
2022-07-20 19:57:22 +0000

    --color-tweak

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid h-screen place-items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div class="grid h-screen place-items-center bg-gradient-to-r from-gray-500 via-gray-700 to-gray-500">
         <div class="text-white">
             <h1 class="text-7xl text-center">{{'<Application />'}}</h1>
             <p class="text-4xl underline decoration-wavy decoration-2 underline-offset-8 whitespace-normal">This will be the start of something great!</p>
